711534385c167e0f485538358e29967450aef2a6
udeshkumar12 <udeshk949@gmail.com>
2026-03-08 05:37:31 -0400

    This is content file

---
+++ content.js
@@ -0,0 +1,15 @@
+1. Let and Const
+2. Arrow Function
+3. Template Literals
+4. Default Parameters
+5. Destructuring (Array or Object)
+6. Spread Operato
+7. Rest Parameter
+8. Classes
+9. Modules (Import / Export)
+10. Promises
+11. Map and Set
+12. For...of Loop
+13. Symbol
+14. Iterators
+15. Generators
